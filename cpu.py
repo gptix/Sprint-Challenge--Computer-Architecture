175dc4edfa89a6a1fd46dee502eb3df941f69ad6
Jud Taylor <jud.taylor@protonmail.com>
2020-07-24 14:21:47 -0400

    removed debugging print commands

--- cpu.py
+++ cpu.py
@@ -361,27 +361,27 @@ class CPU:
     def JNE(self):
 
         FL = self.FL
-        print(f'FL = {FL:3b}')
+        # print(f'FL = {FL:3b}')
 
         EQ_flag = FL & 0b001 # get only the EQ flag (no need to shift)
-        print(f'EQ flag = {EQ_flag}')
+        # print(f'EQ flag = {EQ_flag}')
         EQ_flag_set = EQ_flag == 1
-        print(f'EQ flag_set = {EQ_flag_set}')
+        # print(f'EQ flag_set = {EQ_flag_set}')
         if       not       EQ_flag_set:
             register_with_jump_address = self.ram[self.PC+1]
-            print(f'register_with_jump_address = {register_with_jump_address}')
+            # print(f'register_with_jump_address = {register_with_jump_address}')
 
             jump_address = self.registers[register_with_jump_address]
-            print(f'jump_address = {jump_address}')
+            # print(f'jump_address = {jump_address}')
 
-            print(f'self.PC before assigning jump address= {self.PC}')
+            # print(f'self.PC before assigning jump address= {self.PC}')
             self.PC = jump_address
-            print(f'self.PC after assigning jump address= {self.PC}')
+            # print(f'self.PC after assigning jump address= {self.PC}')
             self.PC -= 2 # compensate for run loop incrementing counter
-            print(f'self.PC adjustung for run loop= {self.PC}')
+            # print(f'self.PC adjustung for run loop= {self.PC}')
 
-        else:
-            print('Flag was set (not clear). Did not Jump.')
+        # else:
+            # print('Flag was set (not clear). Did not Jump.')
 
         return
 
